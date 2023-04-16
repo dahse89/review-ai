86aa65df6a30c5f8fa69d8b9824fc28bba9f25c3
Philipp Dahse <philipp.dahse@check24.de>
2023-04-16 19:25:23 +0200

    feat: Add tailwind and daisyUi

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppComponent } from './app.component';
+import { AiInputComponent } from './ai-input/ai-input.component';
+import { IconsComponent } from './icons/icons.component';
+import { RatingStarsComponent } from './rating-stars/rating-stars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AiInputComponent,
+    IconsComponent,
+    RatingStarsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
